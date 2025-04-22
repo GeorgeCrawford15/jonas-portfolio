17240df58cf22928f66c6eec9d0a66a4046288c2
George Crawford <georgecrawford.999@gmail.com>
2025-04-21 21:35:59 -0400

    format about and contact sections

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const title = document.querySelector("h1");
 const projectsTitle = document.querySelector("h2");
 const projectSection = document.querySelector("#projects");
 const projectContainers = document.querySelectorAll(".project-container");
-
+const projectDescriptions = document.querySelectorAll(".specific-description");
 
 document.addEventListener("DOMContentLoaded", function() {
     title.classList.add("fade-in");
@@ -12,10 +12,16 @@ projectsTitle.addEventListener("mouseenter", function() {
     projectSection.classList.add("fade-in-fast");
 });
 
-projectContainers.forEach(container => {
+projectContainers.forEach((container, index) => {
+    const description = projectDescriptions[index];
+
     container.addEventListener("mouseenter", function() {
         container.classList.add("fade-in-fast");
+
+        if (description) {
+            setTimeout(() => {
+                description.classList.add("slide-in");
+            }, 800)
+        }
     })
 });
-
-
