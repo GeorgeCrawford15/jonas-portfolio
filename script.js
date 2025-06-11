af84903e26770b8728c3a3cb23fadfcb8e16be2e
George Crawford <georgecrawford.999@gmail.com>
2025-06-10 23:31:06 -0400

    fix mobile interactivity and make navbar more visible on mobile devices

--- script.js
+++ script.js
@@ -4,40 +4,42 @@ const projectSection = document.querySelector("#projects");
 const projectContainers = document.querySelectorAll(".project-container");
 const projectDescriptions = document.querySelectorAll(".specific-description");
 
+const MIN_WIDTH = 750;
+
 document.addEventListener("DOMContentLoaded", function() {
     title.classList.add("fade-in");
+    projectsTitle.classList.add("fade-in");
 });
-
 
 projectsTitle.addEventListener("mouseenter", function() {
-    projectSection.classList.add("fade-in-fast");
+    if (window.innerWidth >= MIN_WIDTH) {
+        projectSection.classList.add("fade-in-fast");
+    }
 }); 
 
-projectsTitle.addEventListener("touchstart", function() {
-    projectSection.classList.add("fade-in-fast");
-});
+if (window.innerWidth < MIN_WIDTH) {
+    projectSection.style.opacity = "1";
 
+    projectContainers.forEach((container, index) => {
+        const description = projectDescriptions[index];
+
+        container.style.opacity = "1";
+        description.style.opacity = "1";
+    })
+}
 
 projectContainers.forEach((container, index) => {
     const description = projectDescriptions[index];
 
     container.addEventListener("mouseenter", function() {
+        if (window.innerWidth >= MIN_WIDTH) {
         container.classList.add("fade-in-fast");
 
-        if (description) {
-            setTimeout(() => {
-                description.classList.add("slide-in");
-            }, 400)
-        }
-    })
-
-    container.addEventListener("touchstart", function() {
-        container.classList.add("fade-in-fast");
-
-        if (description) {
-            setTimeout(() => {
-                description.classList.add("slide-in");
-            }, 400)
+            if (description) {
+                setTimeout(() => {
+                    description.classList.add("slide-in");
+                }, 400)
+            }
         }
     })
 });
